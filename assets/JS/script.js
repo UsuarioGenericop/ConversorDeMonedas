d777c00ed188cbbed24fbc7e8370df31c130959e
Andres <andydquintero@gmail.com>
2025-09-02 23:49:30 -0500

    AddedHTMLElemntsToScript

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,12 @@
 const result = document.getElementById("result");
 const currency = document.getElementById("currency");
+const button = document.getElementById("searchButton");
+const input = document.getElementById("quantity");
 
 async function convertCurrency() {
   const res = await fetch("https://mindicador.cl/api/");
   const data = await res.json();
-  console.log(data);
+  console.log(data.bitcoin);
   for (var propt in data) {
     const currency = data[propt]; // If you have an object with properties that are themselves objects, you can iterate through the keys of the outer object and then access the nested objects and their properties.
     // The const currency is an object so if you console.log(currency) it displays an object whereas if you console.log(propt) it displays a property
@@ -12,8 +14,7 @@ async function convertCurrency() {
   }
   return data;
 }
-convertCurrency();
-// async function render() {
-//   const datos = await convertCurrency();
-// }
-// render();
+button.addEventListener("click", function () {
+  console.log(input.value);
+  console.log(data);
+});
